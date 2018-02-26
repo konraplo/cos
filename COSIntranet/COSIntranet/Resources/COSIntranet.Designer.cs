ac917c6552056c8b8821a54c84fef01fe0809b76
Konrad Plocharski <Konrad.Plocharski@wp.pl>
2018-02-26 10:37:42 +0100

    namespace

--- COSIntranet/COSIntranet/Resources/COSIntranet.Designer.cs
+++ COSIntranet/COSIntranet/Resources/COSIntranet.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace COSIntranet.Resources {
+namespace COS.Intranet.Resources {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace COSIntranet.Resources {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("COSIntranet.Resources.COSIntranet", typeof(COSIntranet).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("COS.Intranet.Resources.COSIntranet", typeof(COSIntranet).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
