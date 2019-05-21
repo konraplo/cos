58855acb12ad204e6d5ea4fa9861c0774b6b6d53
Konrad Plocharski <Konrad.Plocharski@wp.pl>
2019-05-21 22:19:18 +0200

    Contracts notification - initial stuff

---
+++ COSIntranet/COSContracts/TimerJobs/ChangeContractsNotificationTimerJob.cs
@@ -0,0 +1,52 @@
+﻿namespace Change.Contracts.TimerJobs
+{
+    using Microsoft.SharePoint.Administration;
+    using System;
+
+    /// <summary>
+    /// This job definition represents the Timer job responsible for the Change contracts notifications
+    /// </summary>
+    public class ChangeContractsNotificationTimerJob : SPJobDefinition
+    {
+        /// <summary>
+        /// Empty CTOR
+        /// </summary>
+        public ChangeContractsNotificationTimerJob() : base()
+        {
+
+        }
+
+        /// <summary>
+        /// Unused CTOR
+        /// </summary>
+        /// <param name="jobName">Name of the job</param>
+        /// <param name="service">The Service</param>
+        /// <param name="server">The server</param>
+        /// <param name="targetType">SPJobLockType</param>
+        public ChangeContractsNotificationTimerJob(string jobName, SPService service, SPServer server, SPJobLockType targetType) : base(jobName, service, server, targetType)
+        {
+
+        }
+
+        /// <summary>
+        /// Unused CTOR
+        /// </summary>
+        /// <param name="jobName">Name of the job</param>
+        /// <param name="webApplication">WebApplication object</param>
+        public ChangeContractsNotificationTimerJob(string jobName, SPWebApplication webApplication) : base(jobName, webApplication, null, SPJobLockType.ContentDatabase)
+        {
+            //this.Title = CommonUtilities.ChangeNotificationTimerJobName;
+        }
+
+        /// <summary>
+        /// Execute-Method.
+        /// </summary>
+        /// <param name="targetInstanceId">ID of the job instance</param>
+        public override void Execute(Guid targetInstanceId)
+        {
+            //Logger.WriteLog(Logger.Category.Information, this.GetType().Name, "Entered Executemethod.");
+            //ChangeNotificationTimerJobExecuter executer = new ChangeNotificationTimerJobExecuter();
+            //executer.Execute(this);
+        }
+    }
+}
