7606dfead7cea6e95974670b6bbf1e134763fdd3
konrad.plocharski <konrad.plocharski@spVM>
2019-10-12 09:26:48 +0000

    Project feature - initial state

---
+++ COSIntranet/COSIntranet/Features/ChangeProjects/ChangeProjects.EventReceiver.cs
@@ -0,0 +1,64 @@
+
+
+namespace Change.Intranet.Features.ChangeProjects
+{
+    using Change.Intranet.Common;
+    using System;
+    using System.Runtime.InteropServices;
+    using Microsoft.SharePoint;
+
+    /// <summary>
+    /// This class handles events raised during feature activation, deactivation, installation, uninstallation, and upgrade.
+    /// </summary>
+    /// <remarks>
+    /// The GUID attached to this class may be used during packaging and should not be modified.
+    /// </remarks>
+
+    [Guid("bb4a92ab-bc88-4890-b21f-dfface5b10dd")]
+    public class ChangeProjectsEventReceiver : SPFeatureReceiver
+    {
+        // Uncomment the method below to handle the event raised after a feature has been activated.
+
+        public override void FeatureActivated(SPFeatureReceiverProperties properties)
+        {
+            SPWeb web = properties.Feature.Parent as SPWeb;
+
+            if (web != null)
+            {
+                Logger.WriteLog(Logger.Category.Information, "ChangeProjectsEventReceiver", "add CT and ER");
+                //AddFieldsCtErToLists(web);
+
+                
+                //Upgradeto12(web);
+                //Upgradeto13(web);
+            }
+        }
+
+
+        // Uncomment the method below to handle the event raised before a feature is deactivated.
+
+        //public override void FeatureDeactivating(SPFeatureReceiverProperties properties)
+        //{
+        //}
+
+
+        // Uncomment the method below to handle the event raised after a feature has been installed.
+
+        //public override void FeatureInstalled(SPFeatureReceiverProperties properties)
+        //{
+        //}
+
+
+        // Uncomment the method below to handle the event raised before a feature is uninstalled.
+
+        //public override void FeatureUninstalling(SPFeatureReceiverProperties properties)
+        //{
+        //}
+
+        // Uncomment the method below to handle the event raised when a feature is upgrading.
+
+        //public override void FeatureUpgrading(SPFeatureReceiverProperties properties, string upgradeActionName, System.Collections.Generic.IDictionary<string, string> parameters)
+        //{
+        //}
+    }
+}
---
+++ COSIntranet/COSIntranet/Features/ChangeProjects/ChangeProjects.feature
@@ -0,0 +1,11 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="74017d62-845c-4f81-8f54-f3ba5e33f309" description="Deploy Change of Scandinavia projects management artifacts (lists, event receivers...)" featureId="74017d62-845c-4f81-8f54-f3ba5e33f309" imageUrl="" receiverAssembly="$SharePoint.Project.AssemblyFullName$" receiverClass="$SharePoint.Type.bb4a92ab-bc88-4890-b21f-dfface5b10dd.FullName$" solutionId="00000000-0000-0000-0000-000000000000" title="Change of Scandinavia projects management artifacts" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="851960c8-e714-478e-95bf-3b0555a1114d" />
+    <projectItemReference itemId="8204a972-4679-478f-8b03-cfac44ee3380" />
+    <projectItemReference itemId="ed0e257c-d386-4846-9642-653118dd633e" />
+    <projectItemReference itemId="d75341f4-eba0-4f76-8dee-8be79e89d331" />
+    <projectItemReference itemId="ecdcfd74-9369-4565-a402-15f7cd09b58c" />
+    <projectItemReference itemId="58f964d4-9be6-4520-8801-f234542b0d27" />
+  </projectItems>
+</feature>
