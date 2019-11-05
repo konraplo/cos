2f8c4d93e3b171b201da2bd6181c2c8e3796ea9b
Konrad Plocharski <Konrad.Plocharski@wp.pl>
2019-11-05 14:54:13 +0100

    fix resx

--- COSIntranet/COSIntranet/Resources/COSIntranet.Designer.cs
+++ COSIntranet/COSIntranet/Resources/COSIntranet.Designer.cs
@@ -19,7 +19,7 @@ namespace Change.Intranet.Resources {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class COSIntranet {
@@ -372,6 +372,15 @@ namespace Change.Intranet.Resources {
         internal static string ChangeCTProjectMGMT {
             get {
                 return ResourceManager.GetString("ChangeCTProjectMGMT", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to CHANGE Project.
+        /// </summary>
+        internal static string ChangeCTProjectMGMTDescr {
+            get {
+                return ResourceManager.GetString("ChangeCTProjectMGMTDescr", resourceCulture);
             }
         }
         
