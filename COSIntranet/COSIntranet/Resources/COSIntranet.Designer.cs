0172e75746e7044e74bdf37bc43720fffc9248b1
konrad.plocharski <konrad.plocharski@spVM>
2020-10-19 16:17:21 +0000

    add new folder translations

--- COSIntranet/COSIntranet/Resources/COSIntranet.Designer.cs
+++ COSIntranet/COSIntranet/Resources/COSIntranet.Designer.cs
@@ -19,7 +19,7 @@ namespace Change.Intranet.Resources {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class COSIntranet {
@@ -1902,6 +1902,15 @@ namespace Change.Intranet.Resources {
         internal static string ChangeFolderTitleSharedLogin {
             get {
                 return ResourceManager.GetString("ChangeFolderTitleSharedLogin", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Shipping report.
+        /// </summary>
+        internal static string ChangeFolderTitleShippingReport {
+            get {
+                return ResourceManager.GetString("ChangeFolderTitleShippingReport", resourceCulture);
             }
         }
         
